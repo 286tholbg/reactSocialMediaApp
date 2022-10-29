f77506c5c460a901f36dfa10774bfa576a5c5995
286tholbg <107855431+286tholbg@users.noreply.github.com>
2022-10-29 15:08:53 +1100

    complete post scss

--- src/components/post/Post.js
+++ src/components/post/Post.js
@@ -1,26 +1,49 @@
 import './post.scss'
+import { Link } from 'react-router-dom';
 import FavoriteBorderOutlinedIcon from '@mui/icons-material/FavoriteBorderOutlined';
 import FavoriteOutlinedIcon from '@mui/icons-material/FavoriteOutlined';
 import TextsmsOutlinedIcon from '@mui/icons-material/TextsmsOutlined';
 import ShareOutlinedIcon from '@mui/icons-material/ShareOutlined';
-import MoreHorizOutlinedIcon from '@mui/icons-material/MoreHorizOutlined';
+import MoreHorizIcon from '@mui/icons-material/MoreHoriz';
 
 
 const Post = ({post}) => {
+   
+    const liked = false
 
     return (
         <div className='post'>
-            <div className='user'>
-
-            </div>
-            <div className='userInfo'>
-
-            </div>
-            <div className='content'>
-
-            </div>
-            <div className='info'>
-
+            <div className='container'>
+                <div className='user'>
+                    <div className='userInfo'>
+                        <img src={post.profilePic}/>
+                        <div className='details'>
+                            <Link to={`/profile/${post.userId}`} style={{textDecoration: "none", color: "inherit"}}>
+                                <span className='name'>{post.name}</span>
+                            </Link>
+                            <span className='date'>1 min ago</span>
+                        </div>
+                    </div>
+                    <MoreHorizIcon />
+                </div>
+                <div className='content'>
+                    <p>{post.desc}</p>
+                    <img src={post.img} alt="" />
+                </div>
+                <div className='info'>
+                    <div className='item'>
+                        {liked ? <FavoriteOutlinedIcon/> : <FavoriteBorderOutlinedIcon /> }
+                        12 likes
+                    </div>
+                    <div className='item'>
+                        <TextsmsOutlinedIcon />
+                        2 Comments
+                    </div>
+                    <div className='item'>
+                       <ShareOutlinedIcon />
+                        7 likes
+                    </div>
+                </div>
             </div>
         </div>
     )
--- src/components/posts/Posts.js
+++ src/components/posts/Posts.js
@@ -10,17 +10,20 @@ const Posts = () => {
             profilePic: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
             desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum, ligula nec fermentum rhoncus, quam sem pellentesque augue",
             img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
+        },
+        {
+            id: 1,
+            name: "Mathew Connor",
+            userId: 1,
+            profilePic: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600",
+            desc: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut dictum, ligula nec fermentum rhoncus, quam sem pellentesque augue",
+            img: "https://images.pexels.com/photos/4881619/pexels-photo-4881619.jpeg?auto=compress&cs=tinysrgb&w=1600"
         }
     ]
     return (
         <div className='Posts'>
             {posts.map(post => (
-                <Post 
-                    key={post.id} 
-                    post={post.name}
-                    profilePic={post.profilePic}
-                    img={post.img}
-                />
+                <Post post={post} key={post.id}/>
             ))}
         </div>
     )
